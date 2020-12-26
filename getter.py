aba2bc35377fdc047bce81e1acd9da973d05501f
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2020-12-26 00:02:32 -0800

    can connect py template with js

--- getter.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-def problem_titles():
-    conn = sqlite3.connect('kattistracker.db')
-    c = conn.cursor()
-    c.execute('''SELECT distinct problem FROM userprofile LIMIT 10''')
-    problems = c.fetchall()
-    conn.close()
-    return problems
--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify
 from scraper import scrape
-import getter
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "../templates", static_folder="../static")
 conn = sqlite3.connect('kattistracker.db')
 c = conn.cursor()
 c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='userprofile' ''')
@@ -15,14 +14,27 @@ if c.fetchone()[0]!=1 :
 conn.commit()
 conn.close()
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
 @app.route('/', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         # scrape(request.form['user-input'], request.form['token-input'])
-        problems = getter.problem_titles()
-        return render_template('stats.html', username = problems, token = 'jfaijf')
+        return render_template('stats.html')
     else:
         return render_template('login.html')
+
+@app.route('/api/problemtitles', methods=['GET'])
+def api_problemtitles():
+    conn = sqlite3.connect('kattistracker.db')
+    conn.row_factory = dict_factory
+    c = conn.cursor()
+    all_problemtitles = c.execute('SELECT distinct problem FROM userprofile LIMIT 10;').fetchall()
+    return jsonify({'problemtitles': all_problemtitles})
 
 if __name__ == "__main__":
     app.run(debug=True)
